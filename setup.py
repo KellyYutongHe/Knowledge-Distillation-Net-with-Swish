eb93efcb0dd14011399237cb3a5886e514f0ec4f
KellyYutongHe <yhe29@u.rochester.edu>
2019-09-29 22:19:25 -0400

    swish function

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup, Extension
 from torch.utils import cpp_extension
 
 setup(name='swish_cpp',
-      ext_modules=[cpp_extension.CppExtension('swish_cpp', ['swish.cpp'])],
+      ext_modules=[cpp_extension.CppExtension('swish_cpp', ['swish_function.cpp'])],
       cmdclass={'build_ext': cpp_extension.BuildExtension})
