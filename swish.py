bb1d52692313bbbec0b3b08de8957e2133cbe06a
KellyYutongHe <yhe29@u.rochester.edu>
2019-09-29 23:15:07 -0400

    swish module modified

--- swish.py
+++ swish.py
@@ -8,23 +8,6 @@ import swish_cpp
 torch.manual_seed(42)
 
 
-# class SWISHFunction(Function):
-#     @staticmethod
-#     def forward(ctx, x, beta):
-#         output = swish_cpp.forward(x, beta)[0]
-#         variables = [x, beta]
-#         ctx.save_for_backward(*variables)
-#
-#         return output
-#
-#     @staticmethod
-#     def backward(ctx, grad_y):
-#         # grad_x, grad_beta = swish_cpp.backward(grad_y, *ctx.saved_variables)
-#         # return grad_x, grad_beta
-#         grad_x = swish_cpp.backward(grad_y, *ctx.saved_variables)[0]
-#         return grad_x, None
-
-
 class SWISHFunction(Function):
     @staticmethod
     def forward(ctx, x, beta):
@@ -42,20 +25,12 @@ class SWISHFunction(Function):
         # return grad_x, None
 
 
-# class SWISH(nn.Module):
-#     def __init__(self, input_features, state_size):
-#         super(LLTM, self).__init__()
-#         self.input_features = input_features
-#         self.state_size = state_size
-#         self.weights = nn.Parameter(
-#             torch.Tensor(3 * state_size, input_features + state_size))
-#         self.bias = nn.Parameter(torch.Tensor(1, 3 * state_size))
-#         self.reset_parameters()
-#
-#     def reset_parameters(self):
-#         stdv = 1.0 / math.sqrt(self.state_size)
-#         for weight in self.parameters():
-#             weight.data.uniform_(-stdv, +stdv)
-#
-#     def forward(self, input, state):
-#         return LLTMFunction.apply(input, self.weights, self.bias, *state)
+class SWISH(nn.Module):
+    def __init__(self, x):
+        super(SWISH, self).__init__()
+        self.x = x
+        self.beta = nn.Parameter(torch.Tensor(1))
+        self.beta.data.uniform_(0, 1.5)
+
+    def forward(self, x):
+        return LLTMFunction.apply(x, self.beta)
