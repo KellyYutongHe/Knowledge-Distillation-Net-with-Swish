199e6e56918e1e8b649081dcfb2bab617ada77c7
KellyYutongHe <yhe29@u.rochester.edu>
2019-09-30 00:25:16 -0400

    add teacher network

--- resnet.py
+++ resnet.py
@@ -3,9 +3,15 @@
     Deep Residual Learning for Image Recognition
     https://arxiv.org/abs/1512.03385v1
 """
-
+from __future__ import print_function
+import argparse
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from util import AverageMeter, accuracy
+from swish import SWISH
 
 class BasicBlock(nn.Module):
     """Basic Block for resnet 18 and resnet 34
@@ -18,7 +24,7 @@ class BasicBlock(nn.Module):
     expansion = 1
 
     def __init__(self, in_channels, out_channels, stride=1):
-        super().__init__()
+        super(BasicBlock, self).__init__()
 
         #residual function
         self.residual_function = nn.Sequential(
@@ -47,7 +53,7 @@ class BasicBlock(nn.Module):
 class ResNet(nn.Module):
 
     def __init__(self, block, num_block, num_classes=100):
-        super().__init__()
+        super(ResNet, self).__init__()
 
         self.in_channels = 64
 
@@ -109,3 +115,118 @@ def resnet34():
     """ return a ResNet 34 object
     """
     return ResNet(BasicBlock, [3, 4, 6, 3])
+
+
+def train(args, model, device, train_loader, criterion, optimizer, epoch):
+    model.train()
+    # losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+
+        prec1, prec5 = accuracy(output.data, target.data, topk=(1, 5))
+        # losses.update(loss.data[0], data.size(0))
+        top1.update(prec1[0], data.size(0))
+        top5.update(prec5[0], data.size(0))
+
+        loss.backward()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)], Loss: {:.6f}, Top1:{:.3f}, Top5:{:.3f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item(), top1.avg, top5.avg))
+
+def test(args, model, device, criterion, test_loader):
+    model.eval()
+    test_loss = 0
+    # correct = 0
+    top1t = AverageMeter()
+    top5t = AverageMeter()
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += criterion(output, target, reduction='sum').item() # sum up batch loss
+
+            prec1, prec5 = accuracy(output.data, target.data, topk=(1, 5))
+            top1t.update(prec1[0], data.size(0))
+            top5t.update(prec5[0], data.size(0))
+
+            # pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
+            # correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Top1:{:.3f}, Top5:{:.3f}\n'.format(
+        test_loss, top1t.avg, top5t.avg))
+
+    # print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    #     test_loss, correct, len(test_loader.dataset),
+    #     100. * correct / len(test_loader.dataset)))
+
+
+def main():
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch CIIFAR100 Example')
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=35, metavar='N',
+                        help='number of epochs to train (default: 35)')
+    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                        help='learning rate (default: 0.1)')
+    parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                        help='SGD momentum (default: 0.5)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+    parser.add_argument('--save-model', action='store_true', default=True,
+                        help='For Saving the current Model')
+    args = parser.parse_args()
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+    # print(torch.cuda.is_available())
+
+    torch.manual_seed(args.seed)
+
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    kwargs = {'num_workers': 8, 'pin_memory': True} if use_cuda else {}
+    train_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR100('./data', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.Resize((32,32)),
+                           transforms.RandomHorizontalFlip(),
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.5, 0.5, 0.5), (0.2, 0.2, 0.2))
+                       ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR100('./data', train=False, transform=transforms.Compose([
+                           transforms.Resize((32,32)),
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.5, 0.5, 0.5), (0.2, 0.2, 0.2))
+                       ])),
+        batch_size=args.test_batch_size, shuffle=True, **kwargs)
+
+
+    model = resnet18().to(device)
+    # model.load_state_dict(torch.load("student_relu.pt"))
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    criterion = nn.CrossEntropyLoss()
+
+    for epoch in range(1, args.epochs + 1):
+        train(args, model, device, train_loader, criterion, optimizer, epoch)
+        test(args, model, device, criterion, test_loader)
+
+    if (args.save_model):
+        torch.save(model.state_dict(),"resnet.pt")
+
+if __name__ == '__main__':
+    main()
